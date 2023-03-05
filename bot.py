27a00d7a811323c58aecd0dfeee185902ab0c165
Mohsin <92655551+mohsinss@users.noreply.github.com>
2023-03-04 16:21:35 -0800

    bot

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 # bot.py
-import os
 import discord
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,6 +13,12 @@ client = discord.Client(command_prefix='!', intents=intents)
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-
+# this is the code we will use first to test the connection
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('Hello Mr. Botface'):
+    await message.channel.send('Howdy Stranger')
 # start the bot
 client.run(TOKEN)
